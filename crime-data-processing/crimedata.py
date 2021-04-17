cb293d20df40e20f47fe95182046af094bb19c44
CORNWINS <henson.jo@northeastern.edu>
2021-04-17 17:15:11 -0400

    basic weights method

--- crime-data-processing/crimedata.py
+++ crime-data-processing/crimedata.py
@@ -4,19 +4,28 @@ import os
 from scipy import stats
 
 df = pd.read_csv("crime.csv")
-streets = []
-class Street:
-    def __init__(self, name, num_crimes):
-        self.name = name
-        self.num_crimes = num_crimes
-        self.weight = 0
-    def init_weight(self):
-        self.weight = self.num_crimes
-
-num_crimes = df['STREET'].value_counts()
-street_names = df['STREET'].drop_duplicates()
-for name in street_names:
-    streets.append(Street(name, num_crimes.get(name)))
-for street in streets:
-    street.init_weight()
-    print(street.weight)
+crimes = []
+class Crime:
+    type = ""
+    posn = (0, 0) 
+    def __init__(self, type, posn):
+        self.type = type
+        self.posn = posn
+posns = df['Location']
+offenses = df['OFFENSE_CODE_GROUP']
+for i in range(len(df)):
+    crimes.append(Crime(offenses[i], eval(posns[i])))
+# radius in degrees
+def get_num_crimes_in_radius(posn, radius):
+    num_crimes_in_radius = 0
+    bad_crime_weight = 0
+    for crime in crimes:
+        dist = np.sqrt((crime.posn[0] - posn[0])**2 + (crime.posn[1] - posn[1])**2)
+        if dist < radius:
+            num_crimes_in_radius = num_crimes_in_radius + 1
+            bad_crime_weight = bad_crime_weight + get_bad_crime_weight(crime)
+    return num_crimes_in_radius
+    
+def get_weight(posn, radius):
+    return get_num_crimes_in_radius(posn, radius)
+print(get_weight((42.3, -71.1), 0.05))
