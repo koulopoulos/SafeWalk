b597254d26b0fc9558c74371e347791432573de4
koulopoulos <00307645@student.necc.edu>
2021-04-17 17:45:54 -0400

    gcp

---
+++ flaskr/gcp.py
@@ -0,0 +1,27 @@
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key="AIzaSyBTx2IzKN5vaJjztKYB4MRYZpGOWlKYm34")
+
+def get_directions(from_d, to_d):
+    # Request directions via public transit
+    now = datetime.now()
+    return gmaps.directions(
+        from_d, to_d, mode="walking", departure_time=now, alternatives=True)
+    
+
+def routes(g_directions):
+    routes = []
+    for g_route in g_directions:
+        route = {
+            "danger": 0,
+            "steps": []
+        }
+        for g_leg in g_route["legs"]:
+            for g_step in g_leg["steps"]:
+                pos = (g_step["start_location"]["lat"], g_step["start_location"]["lng"])
+                route["steps"].append(pos)
+        routes.append(route)
+    return routes
+
+print(routes(get_directions("Northeastern University", "Boston University")))
