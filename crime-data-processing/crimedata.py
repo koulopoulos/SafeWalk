488d475b540da96e19dc2b1de1b5271a7cb7d669
CORNWINS <henson.jo@northeastern.edu>
2021-04-17 16:19:20 -0400

    Streets weighted by number of crimes

--- crime-data-processing/crimedata.py
+++ crime-data-processing/crimedata.py
@@ -3,5 +3,20 @@ import pandas as pd
 import os
 from scipy import stats
 
-data = pd.read_csv("crime.csv")
-print(data)
+df = pd.read_csv("crime.csv")
+streets = []
+class Street:
+    def __init__(self, name, num_crimes):
+        self.name = name
+        self.num_crimes = num_crimes
+        self.weight = 0
+    def init_weight(self):
+        self.weight = self.num_crimes
+
+num_crimes = df['STREET'].value_counts()
+street_names = df['STREET'].drop_duplicates()
+for name in street_names:
+    streets.append(Street(name, num_crimes.get(name)))
+for street in streets:
+    street.init_weight()
+    print(street.weight)
