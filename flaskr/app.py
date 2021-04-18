50192d160e3c74acf3cb1311af02ab68ccacf49d
koulopoulos <00307645@student.necc.edu>
2021-04-18 00:53:29 -0400

    templates

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from . import gcp
 
 app = Flask(__name__)
@@ -14,11 +14,4 @@ def index():
     if request.method == "POST":
         route = gcp.safest_route(request.form["from"], request.form["to"], request.form["time"])
         return redirect(url_for("route", route_data=route))
-    return """
-        <form method="post">
-            <input type=text name=from>
-            <input type=text name=to>
-            <input type=text name=time>
-            <input type=submit value=go>
-        </form>
-    """
+    return render_template("index.html")
