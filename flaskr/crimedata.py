a1d1beb6de710b2524cab81cd7b53858a2205aa0
CORNWINS <henson.jo@northeastern.edu>
2021-04-17 20:22:25 -0400

    added support for time

--- flaskr/crimedata.py
+++ flaskr/crimedata.py
@@ -2,30 +2,32 @@ import numpy as np
 import pandas as pd
 import os
 from scipy import stats
-# nothing
 df = pd.read_csv("crime.csv")
 crimes = []
 class Crime:
     type = ""
     posn = (0, 0) 
-    def __init__(self, type, posn):
+    def __init__(self, type, posn, time):
         self.type = type
         self.posn = posn
+        self.time = time
 posns = df['Location']
 offenses = df['OFFENSE_CODE_GROUP']
+times = df['HOUR']
 for i in range(len(df)):
-    crimes.append(Crime(offenses[i], eval(posns[i])))
+    crimes.append(Crime(offenses[i], eval(posns[i]), times[i]))
     
 # radius in degrees
-def get_num_crimes_in_radius(posn, radius):
+def get_num_crimes_in_radius(posn, radius, time):
     num_crimes_in_radius = 0
     bad_crime_weight = 0
     for crime in crimes:
         dist = np.sqrt((crime.posn[0] - posn[0])**2 + (crime.posn[1] - posn[1])**2)
-        if dist < radius:
+        time_range = (crime.time-3, crime.time+3)
+        if dist < radius and int(time) >= time_range[0] and int(time) <= time_range[1]:
             num_crimes_in_radius = num_crimes_in_radius + 1
     return num_crimes_in_radius
     
-def get_weight(posn, radius):
-    return get_num_crimes_in_radius(posn, radius)
-print(get_weight((42.3, -71.1), 0.05))
+def get_weight(posn, radius, time):
+    return get_num_crimes_in_radius(posn, radius, time)
+
--- flaskr/gcp.py
+++ flaskr/gcp.py
@@ -1,6 +1,6 @@
 import googlemaps
-from . import crimedata
-from . import config
+import crimedata
+import config
 from datetime import datetime
 gmaps = googlemaps.Client(key=config.GCP_API_KEY)
 
@@ -10,7 +10,7 @@ def get_directions(from_d, to_d):
     return gmaps.directions(
         from_d, to_d, mode="walking", departure_time=now, alternatives=True)
 
-def get_routes(g_directions):
+def get_routes(g_directions, time):
     routes = []
     for g_route in g_directions:
         route = {
@@ -20,13 +20,13 @@ def get_routes(g_directions):
         for g_leg in g_route["legs"]:
             for g_step in g_leg["steps"]:
                 pos = (g_step["start_location"]["lat"], g_step["start_location"]["lng"])
-                route["danger"] += crimedata.get_weight(pos, 0.001)
+                route["danger"] += crimedata.get_weight(pos, 0.001, time)
                 route["steps"].append(pos)
         routes.append(route)
     return routes
 
-def safest_route(from_, to_):
-    routes = get_routes(get_directions(from_, to_))
+def safest_route(from_, to_, time):
+    routes = get_routes(get_directions(from_, to_), time)
     lowest = routes[0]
     for route in routes:
         if route["danger"] < lowest["danger"]:
